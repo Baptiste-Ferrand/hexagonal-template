4dfae699f912b35ec8c4c898f8a59103fe1d0351
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2025-07-15 15:28:46 +0200

    create a container how instancies service and env of the project

---
+++ src/container.py
@@ -0,0 +1,30 @@
+import os
+
+from src.domain.lib.security import BcryptPasswordHasher
+from src.domain.services.profile import ProfileService
+
+
+class Container:
+    def __init__(self, env: str | None = None):
+        self.env = env if env is not None else os.getenv("ENV", "dev")
+        self.hasher = BcryptPasswordHasher()
+
+        if self.env in ("dev", "test"):
+            # from src.adapters.inmemory.repositories.profile import InMemoryProfileRepository
+            # self.profile_repo = InMemoryProfileRepository()
+            print("test")
+        else:
+            from src.adapters.sqlalchemy.db import SessionLocal
+            self.SessionFactory = SessionLocal
+
+    def get_profile_service(self):
+        if self.env in ("dev", "test"):
+            repo = self.profile_repo
+        else:
+            from src.adapters.sqlalchemy.repositories.profile import SqlAlchemyProfileRepository
+            session = self.SessionFactory()
+            repo = SqlAlchemyProfileRepository(session)
+        return ProfileService(repo, self.hasher)
+
+
+container = Container()
